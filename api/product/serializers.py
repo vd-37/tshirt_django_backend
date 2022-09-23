89651398b8fe4126f1c09ab6742753d5c0c58f82
Vinayaka D <62375990+vd-37@users.noreply.github.com>
2022-09-23 17:27:49 +0530

    added product_id field in serializers.py

--- api/product/serializers.py
+++ api/product/serializers.py
@@ -9,4 +9,4 @@ class ProductSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Product
-        fields = ('id', 'name', 'description', 'price', 'image', 'category')
+        fields = ('id', 'name', 'description', 'price', 'product_id', 'image', 'category')
